7cd4ce97a442cc91529d578ffbf8e540b4206fbb
Daniel-Alencar <danielalencar746@gmail.com>
2023-07-02 11:46:06 -0300

    Ajustes

--- Collisions.cpp
+++ Collisions.cpp
@@ -7,6 +7,11 @@
 #include <GL/glut.h>
 #include <math.h>
 
+typedef struct {
+	float x;
+	float y;
+} Point;
+
 GLfloat tx = 0;
 GLfloat win = 25;
 
@@ -41,31 +46,20 @@ GLfloat windowYmin, windowYmax;
 
 // Variáveis de referência do plano cartesiano
 int range = 25;
+bool colision = false;
+
+Point pointsNave[3];
+Point poinstAsteroid[10];
 
 // Função para desenhar uma nave
 void DesenhaNave()
 {
+
 	glLineWidth(2);
 	glBegin(GL_LINE_LOOP);
 		glVertex2f(0.0,1.5);
 		glVertex2f(1.0,-1.0);
 		glVertex2f(-1.0,-1.0);
-	glEnd();
-	glPointSize(1);
-	glBegin(GL_POINTS);
-		glVertex2i(0,0);      
-	glEnd();
-}
-
-// Função para desenhar um obstáculo
-void DesenhaObstaculo()
-{
-	glLineWidth(2);
-	glBegin(GL_LINE_LOOP);
-		glVertex2f(+1.0,+1.0);
-		glVertex2f(+1.0,-1.0);
-		glVertex2f(-1.0,-1.0);
-		glVertex2f(-1.0,+1.0);
 	glEnd();
 	glPointSize(1);
 	glBegin(GL_POINTS);
@@ -122,6 +116,79 @@ void PontosDeReferencia() {
     glVertex2f(0.0f, -range);
     
   glEnd();
+}
+
+int orientation(Point p, Point q, Point r) {
+	float val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+	if (val == 0) {
+		return 0;  // Os pontos são colineares
+	} else if (val > 0) {
+		return 1;  // Sentido horário
+	} else {
+		return 2;  // Sentido anti-horário
+	}
+}
+
+int onSegment(Point p, Point q, Point r) {
+	if (q.x <= fmax(p.x, r.x) && q.x >= fmin(p.x, r.x) &&
+			q.y <= fmax(p.y, r.y) && q.y >= fmin(p.y, r.y)) {
+		return 1;  // q está no segmento pr
+	}
+	return 0;
+}
+
+int doIntersect(Point p1, Point q1, Point p2, Point q2) {
+	int o1 = orientation(p1, q1, p2);
+	int o2 = orientation(p1, q1, q2);
+	int o3 = orientation(p2, q2, p1);
+	int o4 = orientation(p2, q2, q1);
+
+	// Caso geral
+	if (o1 != o2 && o3 != o4) {
+		return 1;  // As linhas se intersectam
+	}
+
+	// Casos especiais
+	if (o1 == 0 && onSegment(p1, p2, q1)) {
+		return 1;  // p2 está em p1q1
+	}
+	if (o2 == 0 && onSegment(p1, q2, q1)) {
+		return 1;  // q2 está em p1q1
+	}
+	if (o3 == 0 && onSegment(p2, p1, q2)) {
+		return 1;  // p1 está em p2q2
+	}
+	if (o4 == 0 && onSegment(p2, q1, q2)) {
+		return 1;  // q1 está em p2q2
+	}
+
+	return 0;  // As linhas não se intersectam
+}
+
+bool detectColision(GLfloat x1, GLfloat y1, GLfloat r1,
+										 GLfloat x2,GLfloat y2,GLfloat r2) {
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+	float distance = std::sqrt(dx*dx + dy*dy);
+
+	if (distance > r1 + r2) {
+		// Circulos são disjuntos
+		return false;
+	}
+
+	if (distance < std::abs(r1 - r2)) {
+		// Um círculo está contido dentro de outro
+		return false;
+	}
+
+	if (distance == 0 && r1 == r2) {
+		// Os círculos são idênticos
+		return true;
+	}
+
+	// Os círculos se intersectam
+	return true;
 }
 
 // Função callback de redesenho da janela de visualização
@@ -207,6 +274,20 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 	}             
 }
 
+bool d_colision() {
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 10; j++) {
+			if(doIntersect(
+				pointsNave[i], pointsNave[(i+1)%3],
+				poinstAsteroid[j], poinstAsteroid[(j+1)%10]
+			)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 // Função callback chamada pela GLUT a cada intervalo de tempo
 void Anima(int value)
 {
@@ -242,18 +323,19 @@ void Anima(int value)
 		Ty_Asteroid = +range + maxY_Asteroid;
 	}
 
-	// Move a Nave
-	Tx += xStep;
-	Ty += yStep;
+	colision = d_colision();
+	if(!colision) {
+		// Move a Nave
+		Tx += xStep;
+		Ty += yStep;
 
-	// Move o Asteroid
-	Tx_Asteroid += xStep_Asteroid;
-	Ty_Asteroid += yStep_Asteroid;
-
-	// printf("Asteróide!\n");
-	// printf("Tx: %.2f e Ty: %.2f\n", Tx_Asteroid, Ty_Asteroid);
-	// printf("Nave!\n");
-	// printf("Tx: %.2f e Ty: %.2f\n", Tx, Ty);
+		// Move o Asteroid
+		Tx_Asteroid += xStep_Asteroid;
+		Ty_Asteroid += yStep_Asteroid;
+	} else {
+		Tx = 0;
+		Ty = 0;
+	}
 
 	// Redesenha a Nave em outra posição
 	glutPostRedisplay();
@@ -283,29 +365,23 @@ void Teclado (unsigned char key, int x, int y)
 	if (key == 27)
 		exit(0);
 	
-	switch (key) {
-		case 'w':
-		case 'W':
-			// Lógica para mover para "cima"
-			xStep += +cos(convertDegreesToRadians(angle + 90)) * 0.001f;
-			yStep += +sin(convertDegreesToRadians(angle + 90)) * 0.001f;
-			break;
-		case 'a':
-		case 'A':
-			// Lógica para mover para a esquerda
-			angle+=5;
-			break;
-		case 's':
-		case 'S':
-			// Lógica para mover para "baixo"
-			xStep += -cos(convertDegreesToRadians(angle + 90)) * 0.001f;
-			yStep += -sin(convertDegreesToRadians(angle + 90)) * 0.001f;
-			break;
-		case 'd':
-		case 'D':
-			// Lógica para mover para a direita
-			angle-=5;
-			break;
+	if(key == 'w' || key == 'W') {
+		// Lógica para mover para "cima"
+		xStep += +cos(convertDegreesToRadians(angle + 90)) * 0.001f;
+		yStep += +sin(convertDegreesToRadians(angle + 90)) * 0.001f;
+	}
+	if(key == 'a' || key == 'A') {
+		// Lógica para mover para a esquerda
+		angle+=5;
+	}
+	if(key == 's' || key == 'S') {
+		// Lógica para mover para "baixo"
+		xStep += -cos(convertDegreesToRadians(angle + 90)) * 0.001f;
+		yStep += -sin(convertDegreesToRadians(angle + 90)) * 0.001f;
+	}
+	if(key == 'd' || key == 'D') {
+		// Lógica para mover para a direita
+		angle-=5;
 	}
 }
            
@@ -315,13 +391,54 @@ void Inicializa (void)
 	// Define a cor de fundo da janela de visualização
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+
+
+	Point p1_nave = {+0.0, +1.5};
+	Point p2_nave = {+1.0, -1.0};
+	Point p3_nave = {-1.0, -1.0};
+	pointsNave[0] = p1_nave;
+	pointsNave[1] = p2_nave;
+	pointsNave[2] = p3_nave;
+
+
+
+	float dimension1 = 2.1875;
+	float dimension2 = 4.375;
+	Point p1_asteroid = {+dimension1,+dimension2};
+	Point p2_asteroid = {+dimension2,+dimension1};
+
+	Point p3_asteroid = {+dimension2,-dimension1};
+	Point p4_asteroid = {+dimension1,-dimension2};
+
+	Point p5_asteroid = {+0,-(dimension2 - dimension1)};
+
+	Point p6_asteroid = {-dimension1,-dimension2};
+	Point p7_asteroid = {-dimension2,-dimension1};
+	
+	Point p8_asteroid = {-dimension1,+0};
+
+	Point p9_asteroid = {-dimension2,+dimension1};
+	Point p10_asteroid = {-dimension1,+dimension2};
+	poinstAsteroid[0] = p1_asteroid;
+	poinstAsteroid[1] = p2_asteroid;
+	poinstAsteroid[2] = p3_asteroid;
+	poinstAsteroid[3] = p4_asteroid;
+	poinstAsteroid[4] = p5_asteroid;
+	poinstAsteroid[5] = p6_asteroid;
+	poinstAsteroid[6] = p7_asteroid;
+	poinstAsteroid[7] = p8_asteroid;
+	poinstAsteroid[8] = p9_asteroid;
+	poinstAsteroid[9] = p10_asteroid;
+
+
+
   // Inicialização das variáveis globais
 	xStep = yStep = 0.00f;
 	Tx = Ty = 0.0f;
 
 	xStep_Asteroid = -0.000f;
 	yStep_Asteroid = +0.010f;
-	Tx_Asteroid = Ty_Asteroid = 0.0f;
+	Tx_Asteroid = Ty_Asteroid = 10.0f;
 
 	minX = -7.0f;
 	maxX = +7.0f;
