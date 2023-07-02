630361e85421b44325d925896da4f3815edc2191
Daniel-Alencar <danielalencar746@gmail.com>
2023-07-02 09:50:00 -0300

    Ajustes

--- Collisions.cpp
+++ Collisions.cpp
@@ -12,18 +12,28 @@ GLfloat win = 25;
 
 // Declaração de variáveis globais
 GLfloat angle = 0;
+
 // Variáveis que guardam a translação que será aplicada sobre a nave
 GLfloat Tx;
 GLfloat Ty;
+// Variáveis que guardam a translação que será aplicada sobre o asteróide
+GLfloat Tx_Asteroid;
+GLfloat Ty_Asteroid;
 
 // Variáveis que guardam os valores mínimos de x e y da nave
 GLfloat minX, maxX;
 GLfloat minY, maxY;
+// Variáveis que guardam os valores mínimos de x e y do asteróide
+GLfloat minX_Asteroid, maxX_Asteroid;
+GLfloat minY_Asteroid, maxY_Asteroid;
 
 // Variáveis que guardam o tamanho do incremento nas 
 // direções x e y (número de pixels para se mover a cada intervalo de tempo)
 GLfloat xStep;
 GLfloat yStep;
+
+GLfloat xStep_Asteroid;
+GLfloat yStep_Asteroid;
 
 // Variáveis que guardam a largura e altura da janela
 GLfloat windowXmin, windowXmax;
@@ -63,22 +73,84 @@ void DesenhaObstaculo()
 	glEnd();
 }
 
+// Função para desenhar um asteróide
+void DesenhaAsteroide() {
+
+	float dimension1 = 2.1875;
+	float dimension2 = 4.375;
+
+	glLineWidth(2);
+	glBegin(GL_LINE_LOOP);
+		glVertex2f(+dimension1,+dimension2);
+		glVertex2f(+dimension2,+dimension1);
+
+		glVertex2f(+dimension2,-dimension1);
+		glVertex2f(+dimension1,-dimension2);
+
+		glVertex2f(+0,-(dimension2 - dimension1));
+
+		glVertex2f(-dimension1,-dimension2);
+		glVertex2f(-dimension2,-dimension1);
+		
+		glVertex2f(-dimension1,+0);
+		
+		glVertex2f(-dimension2,+dimension1);
+		glVertex2f(-dimension1,+dimension2);
+	glEnd();
+	glPointSize(1);
+	glBegin(GL_POINTS);
+		glVertex2i(0,0);      
+	glEnd();
+}
+
+void PontosDeReferencia() {
+	glClear(GL_COLOR_BUFFER_BIT);
+    
+	glPointSize(10.0f);
+  glBegin(GL_POINTS);
+    
+    // Desenha pontos de referência
+		// Meio
+    glVertex2f(0.0f, 0.0f);
+
+		// Right and Left
+    glVertex2f(+range, 0.0f);
+    glVertex2f(-range, 0.0f);
+
+		// Top and Down
+    glVertex2f(0.0f, +range);
+    glVertex2f(0.0f, -range);
+    
+  glEnd();
+}
+
 // Função callback de redesenho da janela de visualização
 void Desenha(void)
 {
 	// Muda para o sistema de coordenadas do modelo
 	glMatrixMode(GL_MODELVIEW);
+	// Limpa a janela de visualização com a cor de fundo definida previamente
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
+
 	// Inicializa a matriz de transformação corrente
 	glLoadIdentity();
 	gluOrtho2D(-range, range, -range, range);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	PontosDeReferencia();
      
-	// Limpa a janela de visualização com a cor de fundo definida previamente
-	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	// Inicializa a matriz de transformação corrente
+	glLoadIdentity();
+	gluOrtho2D(-range, range, -range, range);
 
 	// Aplica uma translação sobre a Nave
 	glTranslatef(Tx, Ty, 0.0f);
 
-	// Desenha Nave
+	// Desenha uma rotação sobre a Nave
 	glTranslatef(0.0f,0.0f,0.0f);
 	glRotatef(angle,0.0f,0.0f,1.0f);
 	glTranslatef(0.0f,0.0f,0.0f);
@@ -86,15 +158,19 @@ void Desenha(void)
 	glColor3f(1.0f,1.0f,1.0f);
 	DesenhaNave();
 
+
+
 	// Inicializa a matriz de transformação corrente
 	glLoadIdentity();
 	gluOrtho2D(-range, range, -range, range);
 
-	// Desenha obstáculo
-	glTranslatef(10.0f, 10.0f, 0.0f);
+	// Aplica uma translação sobre o asteroide
+	glTranslatef(Tx_Asteroid, Ty_Asteroid, 0.0f);
 
 	glColor3f(1.0f,1.0f,1.0f);
-	DesenhaObstaculo();
+	DesenhaAsteroide();
+
+
 
 	// Executa os comandos OpenGL 
 	glutSwapBuffers();
@@ -134,25 +210,50 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 // Função callback chamada pela GLUT a cada intervalo de tempo
 void Anima(int value)
 {
+	// Translação da NAVE
 	// Muda a posição quando chega na borda esquerda e direita
 	if((Tx+maxX) > windowXmax) {
 		Tx = -range;
-	}
-	if((Tx+minX) < windowXmin) {
+	} else if((Tx+minX) < windowXmin) {
 		Tx = +range;
 	}
-
 	// Muda a posição quando chega na borda superior e inferior
 	if((Ty+maxY) > windowYmax) {
 		Ty = -range;
-	}
-	if((Ty+minY) < windowYmin) {
+	} else if((Ty+minY) < windowYmin) {
 		Ty = +range;
+	}
+
+	// Translação do ASTERÓIDE
+	// Muda a posição quando chega na borda esquerda e direita
+	if((Tx_Asteroid+maxX_Asteroid) > windowXmax) {
+		printf("Lado direito\n");
+		Tx_Asteroid = -range + minX_Asteroid;
+	} else if((Tx_Asteroid+minX_Asteroid) < windowXmin) {
+		printf("Lado esquerdo\n");
+		Tx_Asteroid = +range + maxX_Asteroid;
+	}
+	// Muda a posição quando chega na borda superior e inferior
+	if((Ty_Asteroid+maxY_Asteroid) > windowYmax) {
+		printf("Lado de cima\n");
+		Ty_Asteroid = -range + minY_Asteroid;
+	} else if((Ty_Asteroid+minY_Asteroid) < windowYmin) {
+		printf("Lado de baixo\n");
+		Ty_Asteroid = +range + maxY_Asteroid;
 	}
 
 	// Move a Nave
 	Tx += xStep;
 	Ty += yStep;
+
+	// Move o Asteroid
+	Tx_Asteroid += xStep_Asteroid;
+	Ty_Asteroid += yStep_Asteroid;
+
+	// printf("Asteróide!\n");
+	// printf("Tx: %.2f e Ty: %.2f\n", Tx_Asteroid, Ty_Asteroid);
+	// printf("Nave!\n");
+	// printf("Tx: %.2f e Ty: %.2f\n", Tx, Ty);
 
 	// Redesenha a Nave em outra posição
 	glutPostRedisplay();
@@ -173,7 +274,7 @@ void TeclasEspeciais(int key, int x, int y) {
 }
 
 double convertDegreesToRadians(double degrees) {
-	return ((2*M_PI * degrees) / 360); 
+	return ((2*M_PI * degrees) / 360);
 }
 
 // Função callback chamada para gerenciar eventos de teclas
@@ -218,14 +319,26 @@ void Inicializa (void)
 	xStep = yStep = 0.00f;
 	Tx = Ty = 0.0f;
 
-	minX = -10.0f;
-	maxX = +10.0f;
-	minY = -10.0f;
-	maxY = +10.0f;
+	xStep_Asteroid = -0.000f;
+	yStep_Asteroid = +0.010f;
+	Tx_Asteroid = Ty_Asteroid = 0.0f;
+
+	minX = -7.0f;
+	maxX = +7.0f;
+	minY = -7.0f;
+	maxY = +7.0f;
+
+	minX_Asteroid = -7.0f;
+	maxX_Asteroid = +7.0f;
+	minY_Asteroid = -7.0f;
+	maxY_Asteroid = +7.0f;
+
 	windowXmin = windowYmin = -40.0f;
 	windowXmax = windowYmax = +40.0f;
     
-	// Exibe mensagem que avisa como interagir 
+	// Exibe mensagem que avisa como interagir
+	printf("windowXmin: %.2f e windowXmax: %.2f\n", windowXmin, windowXmax);
+	printf("windowYmin: %.2f e windowYmax: %.2f\n", windowYmin, windowYmax);
 }
 
 // Programa Principal 
